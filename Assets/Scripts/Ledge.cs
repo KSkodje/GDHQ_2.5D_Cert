9463e78a82f3b4ae27ee439518508bd4bdcc1acb
Kenneth Skodje <kenneths86@gmail.com>
2020-10-09 00:06:40 +0200

    Initial commit

---
+++ Assets/Scripts/Ledge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Ledge : MonoBehaviour
+{
+    [SerializeField] private Transform _grabPos = default;
+    [SerializeField] private Transform _standPos = default;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ledge_Grab_Checker"))
+        {
+            var player = GameObject.FindWithTag("Player").GetComponent<Player>();
+            if (player) player.GrabLedge(_grabPos.position, this);
+        }
+    }
+
+    public Vector3 GetStandPos()
+    {
+        return _standPos.position;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    private Animator _anim;
+    private Ledge _activeLedge = null;
+    
+    [SerializeField] private Transform _ledgegrabChecker = null;
+    
+    [SerializeField] private float _speed = 3.0f;
+    [SerializeField] private float _jumpHeight = 3.0f;
+    private const float Gravity = -9.81f;
+    private Vector3 _direction, _velocity;
+    private bool _isGrounded;
+    private bool _jumping = false;
+
+    private float _hInput;
+    [SerializeField] private bool _grabbingLedge = false;
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        _controller = GetComponent<CharacterController>();
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    private void CalculateMovement()
+    {
+        
+        // No movement while mid-jump
+        
+        if (_controller.isGrounded)
+        {
+            if (_jumping)
+            {
+                _jumping = false;
+                _anim.SetBool("Jumping", _jumping);
+            }
+            
+            _hInput = Input.GetAxisRaw("Horizontal");
+            _direction = new Vector3(0, 0, _hInput);
+            _anim.SetFloat("Speed", Mathf.Abs(_hInput));
+
+            
+            // Flip != mirror
+            if (_hInput != 0)
+            {
+                Vector3 facing = transform.localEulerAngles;
+                facing.y = _hInput < 0 ? 180 : 0;
+                transform.localEulerAngles = facing;
+            }
+            
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _direction.y += _jumpHeight;
+                _jumping = true;
+                _anim.SetBool("Jumping", _jumping);
+            }
+        }
+
+        
+        
+        _direction.y += Gravity * Time.deltaTime;
+
+        if (_grabbingLedge)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _anim.SetTrigger("ClimbUp");
+            }
+        }
+        else if (!_grabbingLedge)
+        {
+            _controller.Move(_direction * _speed * Time.deltaTime);
+        }
+    }
+
+    public void GrabLedge(Vector3 handPos, Ledge currentLedge)
+    {
+        _grabbingLedge = true;
+        _anim.SetBool("GrabbingLedge", _grabbingLedge);
+        transform.position += handPos - _ledgegrabChecker.position;
+        _anim.SetFloat("Speed", 0.0f);
+        _anim.SetBool("Jumping", false);
+
+        _activeLedge = currentLedge;
+    }
+
+
+    public void ClimbUpComplete()
+    {
+        transform.position = _activeLedge.GetStandPos();
+        _grabbingLedge = false;
+        _anim.SetBool("GrabbingLedge", _grabbingLedge);
+    }
+}
